fb7c5f5e730303b99fc0004bbb0949ae0852b5d4
lv <1094749574@qq.com>
2021-08-29 21:24:13 +0800

    feat(init): 引入 parcel 创建项目,初始化 button 组件

---
+++ src/button.vue
@@ -0,0 +1,25 @@
+<template>
+    <button class="lv-button">按钮</button>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss">
+.lv-button {
+    height: var(--button-height);
+    font-size: var(--font-size);
+    padding: 0 1em;
+    border: 1px solid var(--border-color);
+    border-radius: var(--border-radius);
+    background: var(--button-bg);
+    &:hover {
+        border-color: var(--border-color-hover);
+    }
+    &:active {
+        background-color: var(--button-active-bg);
+    }
+    &:focus {
+        outline: none;
+    }
+}
+</style>
